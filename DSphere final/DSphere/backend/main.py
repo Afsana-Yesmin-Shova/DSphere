786e9f4fd31dd5a8bdce71e3c5bdf1295752b773
Afsana-Yesmin-Shova <afsanayesminshova4@gmail.com>
2026-04-22 12:25:23 +0600

    Update main.py

--- DSphere final/DSphere/backend/main.py
+++ DSphere final/DSphere/backend/main.py
@@ -33,7 +33,12 @@ app = FastAPI(
 # ── CORS ──────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_origins=[
+    settings.FRONTEND_URL,
+    "https://dspheree.netlify.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
